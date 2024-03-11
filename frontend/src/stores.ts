b2e60929ba98ad4571ae2917d762ff28de5a533a
Gray Logan <literal.gray@gmail.com>
2024-03-11 10:14:40 -0400

    WIP: Display loading overlay

--- frontend/src/stores.ts
+++ frontend/src/stores.ts
@@ -4,4 +4,5 @@ export const activeForm = writable<number | null>(null);
 export const checkValidity = writable<boolean>(false);
 export const formDataStore = writable<Object>({});
 export const formValidity = writable<boolean>(false);
+export const loading = writable<boolean>(true);
 export const serialNumberList = writable<string[]>([]);
